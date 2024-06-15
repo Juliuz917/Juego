76377691bf6fdfd3d12c114c2d907ab64817d524
Julian <julianruiz917@hotmail.com>
2024-06-15 17:40:49 -0500

    mi primer commit

---
+++ src/graficos/HojaSprites.java
@@ -0,0 +1,26 @@
+package graficos;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class HojaSprites {
+    private final int ancho;
+    private final int alto;
+    public final int [] pixeles;
+    
+    public HojaSprites(final String ruta, final int ancho, final int alto) throws IOException {
+        this.ancho = ancho;
+        this.alto = alto;
+        
+        pixeles = new int [ancho * alto];
+        
+        BufferedImage imagen = ImageIO.read(HojaSprites.class.getResource(ruta));
+        
+        imagen.getRGB(0, 0, ancho, alto, pixeles, 0, ancho);
+    }
+    public int obtenAncho(){
+            return ancho;
+    }    
+}
+
